b062c9ee048316768c93ada9f3b8c503176cfeae
Manuel Tejada <manute32@gmail.com>
2018-01-21 09:45:45 -0400

    ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": 0,
-    }
+  extends: "airbnb",
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "react/require-default-props": 0,
+    "import/no-extraneous-dependencies": 0,
+    "jsx-a11y/anchor-is-valid": 0
+  }
 };
