3478fbb0b36264964c571b3aa4b0e649520bf96a
Manuel Tejada <manute32@gmail.com>
2018-01-21 09:46:19 -0400

    Initial header and footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+`;
+
+const FooterWrapper = styled.div`
+  border-top: 2px solid black;
+  color: black;
+  grid-area: footer;
+  height: 70px;
+  padding: 1.45rem;
+  text-align: center;
+`;
+
+const Footer = () => (
+  <FooterWrapper>
+    <h2>
+      <StyledLink to="/projects/">Selected Projects</StyledLink>
+    </h2>
+  </FooterWrapper>
+);
+
+export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,27 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  border-bottom: 2px solid black;
+  color: black;
+  grid-area: header;
+  height: 70px;
+  padding: 1.45rem;
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+`;
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+  <HeaderWrapper>
+    <h2>
+      <StyledLink to="/">Carla Prida</StyledLink>
+    </h2>
+  </HeaderWrapper>
+);
 
-export default Header
+export default Header;
