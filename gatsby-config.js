6fa86f2b53fee4d9127fdadc6d2bfd3817ec2593
Manuel Tejada <manute32@gmail.com>
2018-01-21 09:45:03 -0400

    Add plugins to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Carla Prida',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-next',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
 };
