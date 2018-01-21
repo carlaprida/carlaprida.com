52cf15f52f38ca2b1983cf91b236f55564bfdb73
Manuel Tejada <manute32@gmail.com>
2018-01-21 09:45:19 -0400

    Typography.js rules

---
+++ src/utils/typography.js
@@ -0,0 +1,21 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '17px',
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    'GT Walsheim Pro',
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: ['Amerigo BT', 'serif'],
+  headerWeight: 'normal',
+  blockMarginBottom: 0,
+  includeNormalize: true,
+});
+
+export default typography;
