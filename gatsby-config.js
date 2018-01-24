01cb9d617295221e33cb203518094a3b619fb119
Manuel Tejada <manute32@gmail.com>
2018-01-24 18:42:04 -0400

    Configure Contentful Gatsby plugin 🔥

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,12 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '89ckr9wpsq4v',
+        accessToken: '915c692805251756d601c0e3db626030aad85aacb813831cf998814b14b93dfa',
+      },
+    },
   ],
 };
