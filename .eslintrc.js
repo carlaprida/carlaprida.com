37ebb893d4d52514ca94d5fa780bfb7e0cde8176
Manuel Tejada <manute32@gmail.com>
2018-01-24 22:48:55 -0400

    Add prettier to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  extends: "airbnb",
-  plugins: ["react", "jsx-a11y", "import"],
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  plugins: ["react", "jsx-a11y", "import", "prettier"],
   rules: {
+    "prettier/prettier": "error",
     "react/jsx-filename-extension": 0,
     "react/require-default-props": 0,
     "import/no-extraneous-dependencies": 0,
